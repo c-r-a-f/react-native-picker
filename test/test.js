304c79fa3bebb7a0f6974af019c7636a5ba8076d
CRAF <craf.post@gmail.com>
2020-08-24 20:19:12 +0900

    add blank test

--- test/test.js
+++ test/test.js
@@ -1,29 +1,3 @@
-import React from 'react'
-import Picker from '../src'
-
-const data = [
-  {
-    label: 'Red',
-    value: 'red',
-  },
-  {
-    label: 'Orange',
-    value: 'orange',
-  },
-  {
-    label: 'Yellow',
-    value: 'yellow',
-  },
-  {
-    label: 'Green',
-    value: 'green',
-  },
-  {
-    label: 'Blue',
-    value: 'blue',
-  },
-  {
-    label: 'Indigo',
-    value: 'indigo',
-  },
-]
+test('true', () => {
+  expect(true).toBe(true)
+})
